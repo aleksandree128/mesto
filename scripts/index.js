d244eb1a9c7927566d1c9e2b64bc21ec10f89672
aleksandree128 <aleksandree128@mail.ru>
2022-01-07 21:35:10 +0500

    mesto-four

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const popup=document.querySelector('.popup');
 const formElement = document.querySelector('.popup__name');
 let nameInput=document.querySelector('.profile__title');
 let jobInput=document.querySelector('.profile__text');
-let namePopupItemOne=document.querySelector('.popup__item_name');
-let namePopupItemTwo=document.querySelector('.popup__item_about-name');
+let namePopupItemOne=document.querySelector('.popup__item_type_name');
+let namePopupItemTwo=document.querySelector('.popup__item_type_about-name');
 
 function togglePopup(event) {
     if(popup.classList.toggle('popup_opened')===true)
